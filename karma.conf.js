abf055c3e2f6e75fdf2ac1614067f910e2f080d1
Szpetny <msteller@GA018576.ads.eso.org>
2017-01-09 15:13:05 +0100

    angular-cli updated

--- karma.conf.js
+++ karma.conf.js
@@ -14,11 +14,11 @@ module.exports = function (config) {
     files: [
       { pattern: './src/test.ts', watched: false }
     ],
-    exclude: [
-       './src/app/testing/*.ts'       
-    ],
     preprocessors: {
       './src/test.ts': ['angular-cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts','tsx']
     },
     remapIstanbulReporter: {
       reports: {
@@ -30,7 +30,9 @@ module.exports = function (config) {
       config: './angular-cli.json',
       environment: 'dev'
     },
-    reporters: ['progress', 'karma-remap-istanbul'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['progress', 'karma-remap-istanbul']
+              : ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
